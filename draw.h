81b36d254e557ad5b9681f4a31ffb7adf240dc9d
JoeSteeb <55957928+JoeSteeb@users.noreply.github.com>
2021-12-26 15:43:16 -0800

    Delete draw.h

--- draw.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOGIC_H_
-#define LOGIC_H_
-
-#define WINDOW_WIDTH 800
-#define WINDOW_HIGHT 600
-
-int drawFunct(SDL_Renderer *renderer, SDL_Event event, int *points, int connect);
-
-#endif // LOGIC_H_
